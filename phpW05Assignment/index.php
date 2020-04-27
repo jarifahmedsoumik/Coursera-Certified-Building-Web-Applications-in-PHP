b06c49bea44c3e69db30418780fce42ae45baada
Jarif Ahmed Soumik <jasoumik@gmail.com>
2020-04-27 07:04:49 +0600

    Initial commit

---
+++ phpW05Assignment/index.php
@@ -0,0 +1,26 @@
+<title>Jarif Ahmed Soumik PHP</title>
+<h1>Jarif Ahmed Soumik PHP</h1>
+<p>
+<?php
+echo'The SHA256 hash of "Jarif Ahmed Soumik" is ' ;
+print hash('sha256', 'Jarif Ahmed Soumik');
+
+?>
+</p>
+<pre>
+ASCII ART:
+
+      **
+     /**
+     /**
+     /**
+     /**
+ **  /**
+//***** 
+ /////  
+
+<a href="check.php">Click here to check the error setting</a>
+<a href="fail.php">Click here to cause a traceback</a>
+
+</pre>
+
